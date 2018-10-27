116d062a5efe707591f68243fe5bdb789576d1f7
SnackKing <allegretti813@gmail.com>
2018-10-27 16:03:02 -0400

    Added sharedPreference

--- app/src/main/java/com/example/alleg/hack2018/utility/DBUtility.java
+++ app/src/main/java/com/example/alleg/hack2018/utility/DBUtility.java
@@ -3,6 +3,7 @@ package com.example.alleg.hack2018.utility;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,6 +11,7 @@ import com.example.alleg.hack2018.MessagesActivity;
 import com.example.alleg.hack2018.models.User;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
 
 public class DBUtility extends AppCompatActivity {
 
@@ -36,6 +38,13 @@ public class DBUtility extends AppCompatActivity {
     public static void syncDatabase(SQLiteDatabase db) {}
 
     public void login(String phone, String password){
+
+        SharedPreferences mPrefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson("Object goes here"); // myObject - instance of MyObject
+        prefsEditor.putString("Key", json);
+        prefsEditor.commit();
 
         Intent intent = new Intent(getApplicationContext(), MessagesActivity.class);
         startActivity(intent);
