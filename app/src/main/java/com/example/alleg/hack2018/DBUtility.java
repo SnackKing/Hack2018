145e0a2024de3db841a50958fe8c4d47673b7d94
SnackKing <allegretti813@gmail.com>
2018-10-27 12:33:15 -0400

    Added firebase database library. Added format for firebase write.

--- app/src/main/java/com/example/alleg/hack2018/DBUtility.java
+++ app/src/main/java/com/example/alleg/hack2018/DBUtility.java
@@ -3,13 +3,20 @@ package com.example.alleg.hack2018;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DBUtility {
+
+   static DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+
 
     public static void insertToDb(SQLiteDatabase db, String table, String nullColumnHack, ContentValues content) {
         db.insert(table, nullColumnHack, content);
 
         // TODO
         // insert to firebase
+        myRef.child(table).child("1").setValue("object");
     }
 
     // TODO
