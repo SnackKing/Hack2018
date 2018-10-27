640b1776337bfe108a526226feeefb5ba82809df
forsman5 <jrforsman@gmail.com>
2018-10-27 11:52:57 -0400

    added db insert contract

--- app/src/main/java/com/example/alleg/hack2018/DBHelper.java
+++ app/src/main/java/com/example/alleg/hack2018/DBHelper.java
@@ -12,9 +12,11 @@ public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(Queries.SQL_CREATE_ENTRIES);
     }
+
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
---
+++ app/src/main/java/com/example/alleg/hack2018/DBUtility.java
@@ -0,0 +1,15 @@
+package com.example.alleg.hack2018;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DBUtility {
+
+    public static void insertToDb(SQLiteDatabase db, String table, String nullColumnHack, ContentValues content) {
+        db.insert(table, nullColumnHack, content);
+
+        // TODO
+        // insert to firebase
+    }
+
+}
