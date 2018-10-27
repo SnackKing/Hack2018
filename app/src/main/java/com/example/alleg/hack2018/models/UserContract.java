612b7088533a6b74e9ca694cb29c5d0dc17bbfdd
forsman5 <jrforsman@gmail.com>
2018-10-27 13:57:43 -0400

    added salt to the db user table

--- app/src/main/java/com/example/alleg/hack2018/models/UserContract.java
+++ app/src/main/java/com/example/alleg/hack2018/models/UserContract.java
@@ -10,6 +10,7 @@ public final class UserContract {
     public static class User implements BaseColumns {
         public static final String TABLE_NAME = "Users";
         public static final String COLUMN_NAME_PASSWORD = "Password";
+        public static final String COLUMN_NAME_SALT = "Salt";
         public static final String COLUMN_NAME_NAME = "Name";
         public static final String COLUMN_NAME_RESIDENT = "Resident";
     }
