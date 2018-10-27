b40d43ceeb8d83000be1d09df72fb9da995f10db
forsman5 <jrforsman@gmail.com>
2018-10-27 15:09:18 -0400

    removed old stuff

--- app/src/main/java/com/example/alleg/hack2018/SignUpActivity.java
+++ app/src/main/java/com/example/alleg/hack2018/SignUpActivity.java
@@ -46,23 +46,6 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class SignUpActivity extends AppCompatActivity  {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
