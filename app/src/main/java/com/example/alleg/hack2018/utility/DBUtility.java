a7955e2e4b8f359f81caf6827702293c32be6619
forsman5 <jrforsman@gmail.com>
2018-10-27 17:27:40 -0400

    basic implementation of sync

--- app/src/main/java/com/example/alleg/hack2018/utility/DBUtility.java
+++ app/src/main/java/com/example/alleg/hack2018/utility/DBUtility.java
@@ -1,9 +1,12 @@
 package com.example.alleg.hack2018.utility;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -21,14 +24,22 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
 public class DBUtility extends AppCompatActivity {
+
+    static ArrayList<User> notSentUsers = new ArrayList<>();
+    static ArrayList<Message> notSentMessages = new ArrayList<>();
+    static ArrayList<Item> notSentItems = new ArrayList<>();
+    static ArrayList<Inventory> notSentInventories = new ArrayList<>();
 
     static DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
 
     public static final String USER_KEY = "currUser";
 
     public static long insertToDb(SQLiteDatabase db, String table, String nullColumnHack, ContentValues content) {
+        DBUtility util = new DBUtility();
+
         long newId = db.insert(table, nullColumnHack, content);
 
         // now for firebase
@@ -38,21 +49,38 @@ public class DBUtility extends AppCompatActivity {
             case UserContract.User.TABLE_NAME:
                 // now to insert this value to the database
 
-                tableRef.child(String.valueOf(newId)).setValue(new User(newId, content));
+                if (util.isConnected()) {
+                    tableRef.child(String.valueOf(newId)).setValue(new User(newId, content));
+                } else {
+                    notSentUsers.add(new User(newId, content));
+                }
+
                 break;
             case MessageContract.Message.TABLE_NAME:
 
-                tableRef.child(String.valueOf(newId)).setValue(new Message(newId, content));
+                if (util.isConnected()) {
+                    tableRef.child(String.valueOf(newId)).setValue(new Message(newId, content));
+                } else {
+                    notSentMessages.add(new Message(newId, content));
+                }
 
                 break;
             case InventoryContract.Inventory.TABLE_NAME:
 
-                tableRef.child(String.valueOf(newId)).setValue(new Inventory(newId, content));
+                if (util.isConnected()) {
+                    tableRef.child(String.valueOf(newId)).setValue(new Inventory(newId, content));
+                } else {
+                    notSentInventories.add(new Inventory(newId, content));
+                }
 
                 break;
             case ItemContract.Item.TABLE_NAME:
 
-                tableRef.child(String.valueOf(newId)).setValue(new Item(newId, content));
+                if (util.isConnected()) {
+                    tableRef.child(String.valueOf(newId)).setValue(new Item(newId, content));
+                } else {
+                    notSentItems.add(new Item(newId, content));
+                }
 
                 break;
         }
@@ -60,8 +88,25 @@ public class DBUtility extends AppCompatActivity {
         return newId;
     }
 
-    // TODO
-    public static void syncDatabase(SQLiteDatabase db) {}
+    // TODO : save changes and deletions
+    public static void syncDatabase(SQLiteDatabase db) {
+        // empty each of the not sent arrays
+        for (User u : notSentUsers) {
+            myRef.child(UserContract.User.TABLE_NAME).child(String.valueOf(u.id)).setValue(u);
+        }
+
+        for (Inventory i : notSentInventories) {
+            myRef.child(InventoryContract.Inventory.TABLE_NAME).child(String.valueOf(i.id)).setValue(i);
+        }
+
+        for (Item i : notSentItems) {
+            myRef.child(ItemContract.Item.TABLE_NAME).child(String.valueOf(i.id)).setValue(i);
+        }
+
+        for (Message m : notSentMessages) {
+            myRef.child(MessageContract.Message.TABLE_NAME).child(String.valueOf(m.id)).setValue(m);
+        }
+    }
 
     public void login(String phone, String password){
 
@@ -83,5 +128,17 @@ public class DBUtility extends AppCompatActivity {
 
     public static int fromByteArray(byte[] bytes) {
         return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    public boolean isConnected() {
+        Context context = getApplicationContext();
+
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        return isConnected;
     }
 }
