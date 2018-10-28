bab155f4e83b9ef24b49f8dc1ee97da22b6d1c4a
Jack Plank <plankinator@gmail.com>
2018-10-28 02:02:56 -0400

    Began bridgefy implementation

--- app/src/main/java/com/example/alleg/hack2018/MessagesActivity.java
+++ app/src/main/java/com/example/alleg/hack2018/MessagesActivity.java
@@ -14,6 +14,14 @@ import android.view.View;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 
+import com.bridgefy.sdk.client.Bridgefy;
+import com.bridgefy.sdk.client.BridgefyClient;
+import com.bridgefy.sdk.client.Device;
+import com.bridgefy.sdk.client.MessageListener;
+import com.bridgefy.sdk.client.RegistrationListener;
+import com.bridgefy.sdk.client.Session;
+import com.bridgefy.sdk.client.StateListener;
+
 import com.example.alleg.hack2018.contracts.MessageContract;
 import com.example.alleg.hack2018.models.Message;
 import com.example.alleg.hack2018.models.User;
@@ -48,6 +56,38 @@ public class MessagesActivity extends AppCompatActivity implements PublicTab.OnF
         User user = gson.fromJson(json, User.class);
         getSupportActionBar().setTitle(user.name + "'s Messages");
 
+
+
+        //Always use the Application context to avoid leaks
+        Bridgefy.initialize(getApplicationContext(), "7c2890f4-a44a-4999-abe2-042e5e3acd21", new RegistrationListener() {
+            @Override
+            public void onRegistrationSuccessful(BridgefyClient bridgefyClient) {
+                // Bridgefy is ready to start
+                MessageListener messageListener = new MessageListener() {
+                    @Override
+                    public void onMessageReceived(com.bridgefy.sdk.client.Message message) {
+                        super.onMessageReceived(message);
+                        //TODO update database here using received message
+
+                    }
+                };
+                StateListener stateListener = new StateListener() {
+                    @Override
+                    public void onDeviceConnected(Device device, Session session) {
+                        super.onDeviceConnected(device, session);
+                        //TODO make getData function for sending data to nearby devices
+                        //com.bridgefy.sdk.client.Message message =new com.bridgefy.sdk.client.Message.Builder().setContent(getData()).setReceiverId(device.getUserId()).build();
+                    }
+                };
+                Bridgefy.start(messageListener, stateListener);
+            }
+
+            @Override
+            public void onRegistrationFailed(int errorCode, String message) {
+                // Something went wrong: handle error code, maybe print the message
+
+            }});
+
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final PagerAdapter adapter = new PagerAdapter
                 (getSupportFragmentManager(), tabLayout.getTabCount());
@@ -81,7 +121,7 @@ public class MessagesActivity extends AppCompatActivity implements PublicTab.OnF
 
         mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
         this.mDbHelp = new DBHelper(this);
-        ArrayList<Message> messageList = Message.getPublicMessages(mDbHelp.getReadableDatabase());
+        ArrayList<com.example.alleg.hack2018.models.Message> messageList = com.example.alleg.hack2018.models.Message.getPublicMessages(mDbHelp.getReadableDatabase());
         mMessageAdapter = new MessageListAdapter(this, messageList);
 
         mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
