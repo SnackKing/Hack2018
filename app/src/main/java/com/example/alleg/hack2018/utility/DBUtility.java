6d6858a36d6e383686a820a905f60d7c1dbbd8e5
forsman5 <jrforsman@gmail.com>
2018-10-27 23:37:18 -0400

    removed special handling for no internet, to be done all with full sync

--- app/src/main/java/com/example/alleg/hack2018/utility/DBUtility.java
+++ app/src/main/java/com/example/alleg/hack2018/utility/DBUtility.java
@@ -23,16 +23,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.Gson;
 
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class DBUtility extends AppCompatActivity {
-
-    static ArrayList<User> notSentUsers = new ArrayList<>();
-    static ArrayList<Message> notSentMessages = new ArrayList<>();
-    static ArrayList<Item> notSentItems = new ArrayList<>();
-    static ArrayList<Inventory> notSentInventories = new ArrayList<>();
 
     static DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
 
@@ -46,6 +40,7 @@ public class DBUtility extends AppCompatActivity {
         this.context = con;
         this.db = new DBHelper(this.context).getWritableDatabase();
         this.sync = new SyncThread(this, db);
+        this.sync.start();
     }
 
     public void insertToDb(String table, String key, String nullColumnHack, ContentValues content) {
@@ -61,9 +56,6 @@ public class DBUtility extends AppCompatActivity {
 
                 if (this.isConnected()) {
                     tableRef.child(String.valueOf(key)).setValue(new User(content));
-                } else {
-                    notSentUsers.add(new User(content));
-                    sync.start();
                 }
 
                 break;
@@ -71,9 +63,6 @@ public class DBUtility extends AppCompatActivity {
 
                 if (this.isConnected()) {
                     tableRef.child(String.valueOf(key)).setValue(new Message(content));
-                } else {
-                    notSentMessages.add(new Message(content));
-                    sync.start();
                 }
 
                 break;
@@ -81,9 +70,6 @@ public class DBUtility extends AppCompatActivity {
 
                 if (this.isConnected()) {
                     tableRef.child(String.valueOf(key)).setValue(new Inventory(content));
-                } else {
-                    notSentInventories.add(new Inventory(content));
-                    sync.start();
                 }
 
                 break;
@@ -91,9 +77,6 @@ public class DBUtility extends AppCompatActivity {
 
                 if (this.isConnected()) {
                     tableRef.child(String.valueOf(key)).setValue(new Item(content));
-                } else {
-                    notSentItems.add(new Item(content));
-                    sync.start();
                 }
 
                 break;
