d79b420dfee310181db24036a6b579a3aaf37a4c
Jack Plank <plankinator@gmail.com>
2018-10-27 21:02:19 -0400

    began working on tabs for messages page

--- app/src/main/java/com/example/alleg/hack2018/utility/DBHelper.java
+++ app/src/main/java/com/example/alleg/hack2018/utility/DBHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 8;
+    private static final int DATABASE_VERSION = 9;
     private static final String DATABASE_NAME = "Disaster.db";
 
     public DBHelper(Context context) {
