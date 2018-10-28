9e45d51f8af5650a49229920f7e7666e4d2893cd
SKhatri1029 <sahil.v.khatri@gmail.com>
2018-10-27 22:09:13 -0400

    Added method to add message to database

--- app/src/main/java/com/example/alleg/hack2018/NewMessageActivity.java
+++ app/src/main/java/com/example/alleg/hack2018/NewMessageActivity.java
@@ -1,7 +1,9 @@
 package com.example.alleg.hack2018;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,19 +12,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.alleg.hack2018.contracts.MessageContract;
+import com.example.alleg.hack2018.contracts.UserContract;
 import com.example.alleg.hack2018.models.User;
+import com.example.alleg.hack2018.utility.DBHelper;
 import com.example.alleg.hack2018.utility.DBUtility;
 import com.google.gson.Gson;
 
-public class NewMessageActivity extends AppCompatActivity {
+import java.util.UUID;
 
+public class NewMessageActivity extends AppCompatActivity {
+    private DBHelper mDbHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
-        TextView phone = findViewById(R.id.recipient_phone);
-        TextView message = findViewById(R.id.message);
+        final TextView phone = findViewById(R.id.recipient_phone);
+        final TextView message = findViewById(R.id.message);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -33,13 +40,38 @@ public class NewMessageActivity extends AppCompatActivity {
                addToDatabase(phone.getText().toString(), message.getText().toString());
             }
         });
+        this.mDbHelp = new DBHelper(this);
     }
+
+    @Override
+    protected void onDestroy() {
+        mDbHelp.close();
+
+        super.onDestroy();
+    }
+
     //TODO Implement
     private void addToDatabase(String phone, String message){
         SharedPreferences mPrefs = getPreferences(MODE_PRIVATE);
         Gson gson = new Gson();
         String json = mPrefs.getString(DBUtility.USER_KEY, null);
         User user = gson.fromJson(json, User.class);
+
+        DBUtility util = new DBUtility(getApplicationContext());
+        SQLiteDatabase db = mDbHelp.getWritableDatabase();
+
+        String key = String.valueOf(UUID.randomUUID());
+
+        ContentValues values = new ContentValues();
+
+        values.put(MessageContract.Message.COLUMN_NAME_USER_ID, user.id);
+        values.put(MessageContract.Message.COLUMN_NAME_DESTINATION_ID, phone);
+        values.put(MessageContract.Message.COLUMN_NAME_MESSAGE, message);
+        values.put(MessageContract.Message._ID, key);
+
+        util.insertToDb(db, MessageContract.Message.TABLE_NAME, key, null, values);
+
+        db.close();
     }
 
 }
