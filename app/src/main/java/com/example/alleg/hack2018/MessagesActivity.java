4020e94d282fe6d600fb4185328ea5058f6fe6e1
SnackKing <allegretti813@gmail.com>
2018-10-27 20:58:50 -0400

    Added classes for recyclerview

--- app/src/main/java/com/example/alleg/hack2018/MessagesActivity.java
+++ app/src/main/java/com/example/alleg/hack2018/MessagesActivity.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.example.alleg.hack2018.contracts.MessageContract;
+import com.example.alleg.hack2018.models.Message;
+import com.example.alleg.hack2018.utility.MessageListAdapter;
+
+import java.util.ArrayList;
+
 public class MessagesActivity extends AppCompatActivity {
 
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,12 @@ public class MessagesActivity extends AppCompatActivity {
                 startActivity(newMessageIntent);
             }
         });
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        ArrayList<Message> messageList = new ArrayList<>();
+        long x = 1;
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
